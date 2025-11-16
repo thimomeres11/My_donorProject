1f155afe88f6351df952218a279ecda4f721cd63
thimomeres <thimomeres@gmail.com>
2025-11-16 20:48:42 +0800

    Wip : simpan pekerjaan sebelum sycn development

--- src/components/atoms/AlreadyHaveAcounts/index.tsx
+++ src/components/atoms/AlreadyHaveAcounts/index.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
-// Tambahkan prop onPress agar bisa digunakan untuk navigasi
-interface Props {
-  onPress?: () => void;
-}
-
-const CreateAccount: React.FC<Props> = ({onPress}) => {
+const AlreadyHaveAcounts = () => {
+  const navigation = useNavigation();
+  const handlePress = () => {
+    navigation.navigate('Login');
+  };
+  // Tambahkan prop onPress agar bisa digunakan untuk navigasi
+  interface Props {
+    onPress?: () => void;
+  }
   return (
     <TouchableOpacity
       activeOpacity={0.7}
       style={styles.wrapper}
-      onPress={onPress}>
+      onPress={handlePress}>
       <Text style={styles.text}>Already Have Accounts</Text>
     </TouchableOpacity>
   );
 };
 
-export default CreateAccount;
+export default AlreadyHaveAcounts;
 
 const styles = StyleSheet.create({
   wrapper: {
